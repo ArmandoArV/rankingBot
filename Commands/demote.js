05157488d24701b1d75e9cf908a66aebc6e748b8
ArmandoArV <armando.arredondo.valle@gmail.com>
2024-07-02 19:50:53 -0600

    Implements promote and demote command

--- Commands/demote.js
+++ Commands/demote.js
@@ -4,7 +4,7 @@ const { EmbedBuilder } = require("discord.js");
 const { channelsId, rolesId, idGrupo } = require("../constants.json");
 
 module.exports = {
-  name: "promote",
+  name: "demote",
   async execute(message) {
     const args = message.content.split(" ");
     const username = args[1];
@@ -12,7 +12,7 @@ module.exports = {
       return message.channel.send("Please provide a username.");
     }
 
-    // Check if the user has the specific role to promote
+    // Check if the user has the specific role to demote
     const requiredRoleId = rolesId.admin;
     if (!message.member.roles.cache.has(requiredRoleId)) {
       return message.channel.send(
@@ -33,33 +33,31 @@ module.exports = {
 
       const userInfo = await noblox.getPlayerInfo(userId);
       const currentRank = await noblox.getRankNameInGroup(idGrupo, userId);
-      const newRank = await noblox.promote(idGrupo, userId);
+      const newRank = await noblox.demote(idGrupo, userId);
       const newRankName = await noblox.getRankNameInGroup(idGrupo, userId);
 
       message.channel.send(
-        `User ${username} has been promoted to ${newRankName}.`
+        `User ${username} has been demoted to ${newRankName}.`
       );
 
-      const promotedEmbed = new EmbedBuilder()
-        .setTitle("Promotion Approved")
+      const demotionEmbed = new EmbedBuilder()
+        .setTitle("Demotion Approved")
         .setDescription(
-          `User: ${userInfo.username}\nRank: ${currentRank} ➜ ${newRankName}\nPromoted by: ${message.author.username}`
+          `User: ${userInfo.username}\nRank: ${currentRank} ➜ ${newRankName}\nDemoted by: ${message.author.username}`
         )
-        .setThumbnail(
-          `https://icons.veryicon.com/png/o/miscellaneous/william/upgrade-13.png`
-        )
-        .setColor("#00FF00");
+        .setThumbnail(`https://static.thenounproject.com/png/3427785-200.png`)
+        .setColor("#FF0000");
 
       const promotionLogsChannel = await message.client.channels.fetch(
-        channelsId.logsPromos
+        channelsId.logsDegrados
       );
-      await promotionLogsChannel.send({ embeds: [promotedEmbed] });
+      await promotionLogsChannel.send({ embeds: [demotionEmbed] });
     } catch (error) {
-      console.error(`Error trying to promote user: ${username}`, error);
+      console.error(`Error trying to demote user: ${username}`, error);
       if (error.message.includes("User not found")) {
         message.reply("User not found on Roblox.");
       } else if (error.message.includes("403")) {
-        message.reply("You do not have permission to promote this user.");
+        message.reply("You do not have permission to demote this user.");
       } else {
         message.reply("There was an error trying to execute that command.");
       }
